52ee457051df86d34af43d75627b5e845bc5acc8
Jason.Ghost <specter.jason@yahoo.com>
2016-07-03 19:43:21 +0800

    Beta Version : FILE_DEBUG, CONSOLE_BEBUG and NONE.
    ADD:
    Setting a value of debug results in every event and action packet being written to the log file.
    REVISE:
    You can define macros: FILE_DEBUG, CONSOLE_BEBUG and NONE to control debug message send position.

--- debug.h
+++ debug.h
@@ -1,3 +1,7 @@
+/*
+	Author : Jason.Ghost
+	Time : 2016/7/3
+*/
 #include <stdio.h>
 #include <Windows.h>
 #include <time.h>
@@ -7,7 +11,10 @@
 #ifndef DEBUG
 #define DEBUG
 
-#define DEBUGING
+// control model that debug on console or in file.
+// priority level of 'FILE_DEBUG' larger than 'CONSOLE_DEBUG'
+#define FILE_DEBUG
+#define CONSOLE_DEBUG
 
 #define DARK_BLACK          0
 #define DARK_BLUE           1
@@ -28,45 +35,88 @@
 
 #define SETCOLOR(back, front) ((back) << 4 | (front))
 
+FILE * DEBUG_FILE = NULL;
+const char DEBUG_FILE_NAME[] = "debug.log";
+
+// must invoke this function on the head of application when you use file to debug!
+inline void InitFileDebug(void) {
+	DEBUG_FILE = fopen(DEBUG_FILE_NAME, "a+");
+	if (DEBUG_FILE == NULL) exit(EXIT_FAILURE);
+}
+
 inline void SetColor(int back, int front) {
     static HANDLE handle = 0;
     if(!handle) handle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(handle, SETCOLOR(back, front));
 }
 
+#ifdef FILE_DEBUG
 #define TIMESTAMP \
     SetColor(LIGHT_BLACK, LIGHT_WHITE); \
     printf("[%d | line(%d)]:", time(0), __LINE__);
+#else
+#define TIMESTAMP \
+    fprintf(DEBUG_FILE, "[%d | line(%d)]:", time(0), __LINE__);
+#endif
 
-#ifndef DEBUGING
-#define CONSOLE_WARNING(msg, ...)
-#define CONSOLE_MESSAGE(msg, ...)
-#define CONSOLE_ERROR  (msg, ...) \
-	exit(EXIT_FAILURE);
+#ifdef FILE_DEBUG
+
+	#define DEBUG_WARNING(msg, ...) \
+		TIMESTAMP \
+		fprintf(DEBUG_FILE, "  "); \
+		fprintf(DEBUG_FILE, msg, ## __VA_ARGS__); \
+		fprintf(DEBUG_FILE, "\n");
+
+	#define DEBUG_MESSAGE(msg, ...) \
+		TIMESTAMP \
+		fprintf(DEBUG_FILE, "  "); \
+		fprintf(DEBUG_FILE, msg, ## __VA_ARGS__); \
+		fprintf(DEBUG_FILE, "\n");
+
+	#define DEBUG_ERROR(msg, ...) \
+		TIMESTAMP \
+		fprintf(DEBUG_FILE, "  "); \
+		fprintf(DEBUG_FILE, msg,## __VA_ARGS__); \
+		fprintf(DEBUG_FILE, "\n"); \
+		exit(EXIT_FAILURE);
+
 #else
 
-#define CONSOLE_WARNING(msg, ...) \
-	TIMESTAMP \
-	SetColor(DARK_BLACK, DARK_YELLOW); \
-	printf("  "); \
-	printf(msg, ## __VA_ARGS__); \
-	putchar('\n');
+#ifndef CONSOLE_DEBUG
 
-#define CONSOLE_MESSAGE(msg, ...) \
-	TIMESTAMP \
-	SetColor(DARK_BLACK, DARK_WHITE); \
-	printf("  "); \
-	printf(msg, ## __VA_ARGS__); \
-	putchar('\n');
+	#define DEBUG_WARNING(msg, ...)
+	#define DEBUG_MESSAGE(msg, ...)
+	#define DEBUG_ERROR  (msg, ...) \
+		exit(EXIT_FAILURE);
 
-#define CONSOLE_ERROR(msg, ...) \
-	TIMESTAMP \
-	SetColor(DARK_BLACK, DARK_RED); \
-	printf("  "); \
-	printf(msg,## __VA_ARGS__); \
-	putchar('\n'); \
-	getch(); \
-    exit(EXIT_FAILURE);
+#else
+
+	#define DEBUG_WARNING(msg, ...) \
+		TIMESTAMP \
+		SetColor(DARK_BLACK, DARK_YELLOW); \
+		printf("  "); \
+		printf(msg, ## __VA_ARGS__); \
+		putchar('\n');
+
+	#define DEBUG_MESSAGE(msg, ...) \
+		TIMESTAMP \
+		SetColor(DARK_BLACK, DARK_WHITE); \
+		printf("  "); \
+		printf(msg, ## __VA_ARGS__); \
+		putchar('\n');
+
+	#define DEBUG_ERROR(msg, ...) \
+		TIMESTAMP \
+		SetColor(DARK_BLACK, DARK_RED); \
+		printf("  "); \
+		printf(msg,## __VA_ARGS__); \
+		putchar('\n'); \
+		getch(); \
+		exit(EXIT_FAILURE);
+
+
+#endif
+
 #endif
 
 #endif
